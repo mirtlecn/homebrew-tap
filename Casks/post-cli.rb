74e69ada139102a98fe6f8f3090428da57f6c66d
mirtlecn <mirtle.cn@outlook.com>
2026-06-13 14:20:43 +0800

    Update post-cli version and SHA256 checksums

--- Casks/post-cli.rb
+++ Casks/post-cli.rb
@@ -1,22 +1,22 @@
 cask "post-cli" do
-  version "1.5.0"
+  version "1.5.1"
 
   on_intel do
-    sha256 "c0571a91489bdf44594eebe2d3a0aba52103dcf3a1d010fb610dc1fb70f53f4f"
+    sha256 "6cddf3fd7413579c023e7e1ead3ad65c2b3a454c5be47756c06985a0261f73b9"
 
     url "https://github.com/mirtlecn/post-cli/releases/download/v#{version}/post_#{version}_darwin_amd64.tar.gz",
         verified: "github.com/mirtlecn/post-cli/releases/download/"
   end
 
   on_arm do
-    sha256 "e422a13e4fdf7cfaa44241f6394dcb09bc67c9765a52f97b4b470aaff38aca54"
+    sha256 "a18701317035b0631fd85c1c281989004d4a65687fe2056b4b3231ca73c86ae8"
 
     url "https://github.com/mirtlecn/post-cli/releases/download/v#{version}/post_#{version}_darwin_arm64.tar.gz",
         verified: "github.com/mirtlecn/post-cli/releases/download/"
   end
 
   name "post-cli"
-  desc "Command-line client for post"
+  desc "Command-line client for Post"
   homepage "https://github.com/mirtlecn/post-cli"
 
   livecheck do
